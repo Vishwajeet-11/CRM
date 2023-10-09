9623a3a8b3690849564f9a5a4148be2493ae0e35
Vishwajeet-11 <vishwajeetbharadiya12@gmail.com>
2023-10-09 10:27:28 +0530

    comment out hikari dependency in application-prod.properties  and PostgresDatasource .java file

--- src/main/java/com/agiklo/oracledatabase/configuration/PostgresDatasource.java
+++ src/main/java/com/agiklo/oracledatabase/configuration/PostgresDatasource.java
@@ -1,20 +1,20 @@
-package com.agiklo.oracledatabase.configuration;
+// package com.agiklo.oracledatabase.configuration;
 
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import com.zaxxer.hikari.HikariDataSource;
+// import org.springframework.boot.context.properties.ConfigurationProperties;
+// import org.springframework.boot.jdbc.DataSourceBuilder;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class PostgresDatasource {
+// @Configuration
+// public class PostgresDatasource {
 
-    @Bean
-    @ConfigurationProperties("spring.datasource")
-    public HikariDataSource hikariDataSource() {
-        return DataSourceBuilder
-                .create()
-                .type(HikariDataSource.class)
-                .build();
-    }
-}
+//     @Bean
+//     @ConfigurationProperties("spring.datasource")
+//     public HikariDataSource hikariDataSource() {
+//         return DataSourceBuilder
+//                 .create()
+//                 .type(HikariDataSource.class)
+//                 .build();
+//     }
+// }
